da459731702aa806b4c3aeb644a056afcf866e88
Stephen Denham <stephen.denham@gmail.com>
2026-07-17 17:03:06 -0700

    Image improvements

--- docs/assets/image.js
+++ docs/assets/image.js
@@ -7,15 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const zoomed = img.classList.contains('is-zoomed');
 
-      imgs.forEach(i => i.classList.remove('is-zoomed'));
+      imgs.forEach(i => {
+        i.classList.remove('is-zoomed');
+        if (i.style.zIndex == "60")
+          i.style.zIndex = "30";
+        if (i.style.zIndex == "70")
+          i.style.zIndex = "20";
+      });
 
       if (!zoomed) {
         img.classList.add('is-zoomed');
-        if (img.style.zIndex == '30')
-          img.style.zIndex = '60';
-      } else {
-        if (img.style.zIndex == '60')
-          img.style.zIndex = '30';
+        if (img.style.zIndex == "30")
+          img.style.zIndex = "60";
+        if (img.style.zIndex == "20")
+          img.style.zIndex = "70";
       }
     });
   });
