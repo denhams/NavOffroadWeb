554cf4fbb86d33e22d82e8b1b4fd0b831a7ce6d9
Stephen Denham <stephen.denham@gmail.com>
2026-08-26 14:53:20 -0700

    Google Ads conversion

--- docs/assets/google.js
+++ docs/assets/google.js
@@ -3,3 +3,18 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'AW-16958382109');
+
+function gtag_report_app_store_click(url) {
+  var callback = function () {
+    if (typeof url !== 'undefined') {
+      window.location = url;
+    }
+  };
+
+  gtag('event', 'conversion', {
+    'send_to': 'AW-16958382109/p2cGCN-gs-gcEJ3AsZY_',
+    'event_callback': callback
+  });
+
+  return false;
+}
